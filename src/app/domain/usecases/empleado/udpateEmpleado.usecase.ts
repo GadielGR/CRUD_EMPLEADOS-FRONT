c3b43dc99c40d90624c633c6a612ac1efdc2728d
Gadiel GR <gadielgr@outlook.com>
2023-02-23 13:06:20 -0600

    [add] casos de uso empleado

---
+++ src/app/domain/usecases/empleado/udpateEmpleado.usecase.ts
@@ -0,0 +1,11 @@
+import { UseCase } from 'src/base/use-case';
+import { Empleado } from '../../models/empleado.model';
+import { Observable } from 'rxjs';
+import { EmpeladoRepository } from '../../repositories/empleado.repository';
+
+export class UpdateEmpleadoUseCase implements UseCase<Empleado, Empleado> {
+  constructor(private empleadoRepository: EmpeladoRepository) {}
+  execute(param: Empleado): Observable<Empleado> {
+    return this.empleadoRepository.update(param);
+  }
+}
